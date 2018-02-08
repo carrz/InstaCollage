211abef5e9fa8dcf2ff1cc2bd629edbe8c0c9522
carrz <carla.zumaran@gmail.com>
2018-02-08 09:09:16 -0300

    Arreglo función botón

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,70 +1,9 @@
-/* function handleDragStart(e) {
-  this.style.opacity = '0.4';  // this / e.target is the source node.
-  dragSrcEl = this;
-
-  e.dataTransfer.effectAllowed = 'move';
-  e.dataTransfer.setData('text/html', this.innerHTML);
-}
-
-var photo = document.querySelectorAll('.photo');
-[].forEach.call(photo, function(pic) {
-  pic.addEventListener('dragstart', handleDragStart, false);
-});
-
-var collage = document.querySelectorAll('.collageZone');
-[].forEach.call(collage, function(col) {
-  col.addEventListener('dragenter', handleDragEnter, false);
-  col.addEventListener('dragover', handleDragOver, false);
-  col.addEventListener('dragleave', handleDragLeave, false);
-  col.addEventListener('drop', handleDrop, false);
-  col.addEventListener('dragend', handleDragEnd, false);
-});
-
-function handleDragOver(e) {
-  if (e.preventDefault) {
-    e.preventDefault(); // Necessary. Allows us to drop.
-  }
-  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
-  return false;
-}
-
-function handleDragEnter(e) {
-  // this / e.target is the current hover target.
-  this.classList.add('over');
-}
-
-function handleDragLeave(e) {
-  this.classList.remove('over');  // this / e.target is previous target element.
-}
-
-function handleDrop(e) {
-  // this / e.target is current target element.
-  if (e.stopPropagation) {
-    e.stopPropagation(); // stops the browser from redirecting.
-  }
-  if (dragSrcEl != this) {
-    // Set the source column's HTML to the HTML of the columnwe dropped on.
-    dragSrcEl.innerHTML = this.innerHTML;
-    this.innerHTML = e.dataTransfer.getData('text/html');
-  }
-  // See the section on the DataTransfer object.
-  return false;
-}
-
-function handleDragEnd(e) {
-  // this/e.target is the source node.
-  [].forEach.call(collage, function (col) {
-    col.classList.remove('over');
-    console.log('ya me fui') 
-  });
-} */
-
 // Inicio de sesión
 
 $(document).ready(function(){
 })
 
-$('#name').blur(function() {  
+$('#name').change(function() {  
   if($('#name').val().length < 1) {  
       alert('Su nombre de usuario no puede estar vacío');  
   } else {
@@ -84,9 +23,9 @@ $('#password').blur(function() {
   }
 })
 
-$('#here').mousemove(function() {
+$('#here').mouseover(function() {
   if($('#name').hasClass('success') && $('#password').hasClass('success')){
-    $('#loginBtn').removeClass('disabled');
+    $('#loginBtn').removeAttr('disabled');
   }
 })
 
